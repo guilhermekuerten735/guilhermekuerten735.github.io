2a4414c8ae49fd391f33c2368cd42781c40547fa
Guilherme Kuerten <guilhermekuerten735@gmail.com>
2025-05-07 12:17:22 -0300

    ajuste media

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
-// Adicione aqui funcionalidades JS no futuro
-console.log("Site carregado com sucesso.");
+// Animação da seção "Quem Somos"
+ScrollReveal().reveal('#quem-somos', {
+  origin: 'bottom',
+  distance: '50px',
+  duration: 1000,
+  delay: 100,
+  easing: 'ease-in-out',
+  reset: false
+});
+
+// Animação da seção "Serviços"
+ScrollReveal().reveal('#servicos', {
+  origin: 'bottom',
+  distance: '50px',
+  duration: 1000,
+  delay: 100,
+  easing: 'ease-in-out',
+  reset: false
+});
+
+// Animação da seção "Soluções"
+ScrollReveal().reveal('.secao-solucoes', {
+  origin: 'bottom',
+  distance: '50px',
+  duration: 1000,
+  delay: 100,
+  easing: 'ease-in-out',
+  reset: false
+});
+
+// Animação da seção "Contato / WhatsApp"
+ScrollReveal().reveal('#contato', {
+  origin: 'bottom',
+  distance: '50px',
+  duration: 1000,
+  delay: 100,
+  easing: 'ease-in-out',
+  reset: false
+});
+// Animação do botão de WhatsApp fixo
+ScrollReveal().reveal('.contato-whatsapp', {
+  origin: 'right',  // O botão vai aparecer da direita
+  distance: '100px',
+  duration: 1000,
+  delay: 100,
+  easing: 'ease-in-out',
+  reset: false
+});
